90e7378fb800ee06ae861054a31b9c5fbc5d355f
Sergei Beilin <sergei.beilin@fyndiq.com>
2018-09-21 09:00:46 +0200

    Sketch producer/consumer

---
+++ kafkian/consumer.py
@@ -0,0 +1,90 @@
+import socket
+from typing import Callable
+
+import structlog
+from confluent_kafka import Consumer as ConfluentConsumer
+from confluent_kafka import KafkaException
+
+logger = structlog.get_logger(__name__)
+
+
+class Consumer:
+
+    DEFAULT_CONFIG = {
+        'api.version.request': True,
+        'client.id': socket.gethostname(),
+        'default.topic.config': {
+            'auto.offset.reset': 'latest'
+        },
+        'enable.auto.commit': False,
+        'fetch.error.backoff.ms': 0,
+        'fetch.wait.max.ms': 10,
+        'log.connection.close': False,
+        'log.thread.name': False,
+        'session.timeout.ms': 6000,
+        'statistics.interval.ms': 15000
+    }
+
+    def __init__(
+        self, config,
+            value_serializer=Deserializer(), key_serializer=Deserializer(),
+            get_message: Callable = get_message,
+        error_handler: Callable = default_error_handler
+    ) -> None:
+        stop_on_eof = config.pop('stop_on_eof', False)
+        poll_timeout = config.pop('poll_timeout', 0.1)
+        self.non_blocking = config.pop('non_blocking', False)
+        self.topics = self._get_topics(self.config)
+
+        logger.info("Initializing consumer", config=self.config)
+        self._consumer_impl = ConfluentConsumer(self.config)
+        self._generator = self._message_generator()
+
+    def __getattr__(self, name):
+        return getattr(self.consumer, name)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return next(self._generator)
+        except KafkaException:
+            raise StopIteration
+
+    def __enter__(self):
+        self._consumer_impl.subscribe(self.topics)
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        # the only reason a consumer exits is when an
+        # exception is raised.
+        #
+        # close down the consumer cleanly accordingly:
+        #  - stops consuming
+        #  - commit offsets (only on auto commit)
+        #  - leave consumer group
+        logger.info("Closing consumer")
+        self.consumer.close()
+
+    def _message_generator(self):
+        while True:
+            message = self._consumer_impl.poll(timeout=self.timeout)
+            if message is None:
+                if self.non_blocking:
+                    yield None
+                continue
+            yield Message(message)
+
+    def _get_topics(self, config):
+        topics = config.pop('topics', None)
+        assert topics is not None, "You must subscribe to at least one topic"
+
+        if not isinstance(topics, list):
+            topics = [topics]
+
+        return topics
+
+    @property
+    def is_auto_commit(self):
+        return self.config.get('enable.auto.commit', True)
---
+++ kafkian/producer.py
@@ -0,0 +1,69 @@
+import atexit
+import socket
+
+import structlog
+from confluent_kafka import Producer as ConfluentProducer
+
+from .serialization import Serializer
+
+
+logger = structlog.get_logger(__name__)
+
+
+class Producer:
+    """
+    Kafka producer with configurable key/value serializers.
+
+    Does not subclass directly from Confluent's Producer,
+    since it's a cimpl and therefore not mockable.
+    """
+
+    DEFAULT_CONFIG = {
+        'acks': 'all',
+        'api.version.request': True,
+        'client.id': socket.gethostname(),
+        'log.connection.close': False,
+        'max.in.flight': 1,
+        'queue.buffering.max.ms': 100,
+        'statistics.interval.ms': 15000,
+    }
+
+    def __init__(self, config,
+                 value_serializer=Serializer(), key_serializer=Serializer(),
+                 get_callback=None):  # yapf: disable
+        config = {**self.DEFAULT_CONFIG, **config}
+        self.value_serializer = value_serializer
+        self.key_serializer = key_serializer
+
+        logger.info("Initializing producer", config=config)
+        atexit.register(self._close)
+
+        self._producer_impl = self._init_producer_impl(config)
+
+    @staticmethod
+    def _init_producer_impl(config):
+        return ConfluentProducer(config)
+
+    def _close(self):
+        self.flush()
+
+    def flush(self, timeout=None):
+        logger.info("Flushing producer")
+        if timeout:
+            self._producer_impl.flush(timeout)
+        else:
+            self._producer_impl.flush()
+
+    def poll(self, timeout=None):
+        if timeout:
+            return self._producer_impl.poll(timeout)
+        else:
+            return self._producer_impl.poll()
+
+    def produce(self, topic, key, value):
+        value = self.value_serializer.serialize(value, topic)
+        key = self.key_serializer.serialize(key, topic, is_key=True)
+        self._produce(topic, key, value)
+
+    def _produce(self, topic, key, value, **kwargs):
+        self._producer_impl.produce(topic=topic, value=value, key=key, **kwargs)
