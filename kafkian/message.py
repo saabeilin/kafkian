6cf150fab676c32f3a5aef3922c2c111141920ad
Sergei Beilin <saabeilin@gmail.com>
2019-09-16 13:12:25 +0200

    Improve Message timestamp handling, add docstrings to Message

--- kafkian/message.py
+++ kafkian/message.py
@@ -1,9 +1,22 @@
-from confluent_kafka.cimpl import Message as ConfluentKafkaMessage
+from typing import Optional
+
+from confluent_kafka.cimpl import Message as ConfluentKafkaMessage, TIMESTAMP_NOT_AVAILABLE
 
 from kafkian.serde.deserialization import Deserializer
 
 
 class Message:
+    """
+    Message is an object (log record) consumed from Kafka.
+
+    It provides read-only access to key, value, and message metadata:
+    topic, partition, offset, and optionally timestamp and headers.
+
+    Key and value and deserialized on first access.
+
+    This class wraps cimpl.Message from confluent_kafka
+    and not supposed to be user-instantiated.
+    """
     def __init__(self, 
                  message: ConfluentKafkaMessage, 
                  key_deserializer: Deserializer,
@@ -16,6 +29,9 @@ class Message:
     
     @property
     def key(self):
+        """
+        :return: Deserialized message key
+        """
         if self._deserialized_key:
             return self._deserialized_key
         if self._message.key() is None:
@@ -25,6 +41,9 @@ class Message:
     
     @property
     def value(self):
+        """
+        :return: Deserialized message value
+        """
         if self._deserialized_value:
             return self._deserialized_value
         if self._message.value() is None:
@@ -33,21 +52,50 @@ class Message:
         return self._deserialized_value
 
     @property
-    def topic(self):
+    def topic(self) -> str:
+        """
+        :return: Message topic
+        """
         return self._message.topic()
 
     @property
-    def partition(self):
+    def partition(self) -> int:
+        """
+        :return: Message partition
+        """
         return self._message.partition()
 
     @property
-    def offset(self):
+    def offset(self) -> int:
+        """
+        :return: Message offset
+        """
         return self._message.offset()
 
     @property
-    def timestamp(self):
-        return self._message.timestamp()
+    def timestamp(self) -> Optional[int]:
+        """
+        :return: Message timestamp, of None if not available.
+        """
+        if not self._message.timestamp():
+            return None
+        if self._message.timestamp()[0] == TIMESTAMP_NOT_AVAILABLE:
+            return None
+        return self._message.timestamp()[1]
 
     @property
-    def headers(self):
-        return self._message.headers()
+    def timestamp_type(self) -> Optional[int]:
+        """
+        :return: Message timestamp type - either message creation time or Log Append time, of None if not available.
+        """
+        if not self._message.timestamp():
+            return None
+        return self._message.timestamp()[0]
+
+    @property
+    def headers(self) -> list:
+        """
+        :return: Message headers as list of two-tuples, one (key, value) pair for each header.
+        :rtype: [(str, bytes),...] or None.
+        """
+        return self._message.headers() or []
